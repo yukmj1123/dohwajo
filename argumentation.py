3ce0a5eb7eac79c0cc46a642caab3077b654360b
tjddnr5932 <tjddnr5932@naver.com>
2022-05-22 00:17:19 +0900

    Argumentation 기법을 사용하여 데이터를 증강한다.

---
+++ argumentation.py
@@ -0,0 +1,6 @@
+from numpy import expand_dims
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot as plt
+
