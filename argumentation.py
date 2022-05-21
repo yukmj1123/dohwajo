ee3a4bc937da342c23527ad37c6db6f6996c251f
tjddnr5932 <tjddnr5932@naver.com>
2022-05-22 00:28:35 +0900

    왼쪽, 오른쪽으로 이동하는 argumentation 기법

--- argumentation.py
+++ argumentation.py
@@ -4,3 +4,19 @@ from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot as plt
 
+# Width_Shift_Range : 왼쪽 오른쪽으로 이동
+def Width_Shift_Range(data):
+    samples = expand_dims(data,0)
+    datagen = ImageDataGenerator(width_shift_range=[-200,200])
+
+    it = datagen.flow(samples, batch_size=1)
+
+    fig = plt.figure(figsize=(30,30))
+
+    for i in range(9):
+        plt.subplot(3, 3, i+1)
+        batch = it.next()
+        image = batch[0].astype('uint8')
+        plt.imshow(image)
+
+    plt.show()
